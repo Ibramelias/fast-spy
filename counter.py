ead323c47ffe7685b4a546e8800e0f5d960ea5aa
Ibrameias <ibramdarwish@gmail.com>
2025-07-05 14:23:36 -0500

    create a small counter app

---
+++ counter.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+#function definitions
+def increment():
+    global count
+    count += 1
+    label.config(text=str(count))
+
+
+def decrement():
+    global count
+    count -= 1
+    label.config(text=str(count))
+
+
+def reset():
+    global count
+    count = 0
+    label.config(text=str(count))
+
+#App window
+app = tk.Tk()
+app.title=("Counter App")
+app.geometry("250x150")
+
+count = 0
+
+label = tk.Label(app, text="0", font=("Arial", 32))
+label.pack(pady=10)
+
+frame = tk.Frame(app)
+frame.pack()
+
+tk.Button(frame, text="Increment", command=increment).grid(row=0, column=0)
+tk.Button(frame, text="Decrement", command=decrement).grid(row=0, column=1)
+tk.Button(app, text="Reset", command=reset).pack(pady=5)
+
+
+
+app.mainloop()
