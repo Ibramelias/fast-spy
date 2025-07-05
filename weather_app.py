9e6ac1f85fe815383acad3e2a1d505e70ff67cd4
Ibrameias <ibramdarwish@gmail.com>
2025-07-05 13:25:53 -0500

    create a function to call the weather api

---
+++ weather_app.py
@@ -0,0 +1,28 @@
+import requests
+
+#function to call weather API
+def get_weather(city, api_key):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        'q': city,
+        'appid': api_key,
+        'units': 'metric'
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        main = data['main']
+        weather = data['weather'][0]
+
+        print(f"\nWeather in {city.title()}: ")
+        print(f" Temperature: {main['temp']} C")
+        print(f"Humidity: {main['humidity']} %")
+        print(f"Condition: {weather['description'].title()}")
+    else:
+        print("City not found ar API error")
+
+        
+
+
